c12d2a3254cf48fa0a3a74f56ca3737f7df672e0
chen1393725964 <1393725964@qq.com>
2020-02-21 14:40:28 +0800

    项目部的初始化

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,8 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        APP组件
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'
+function Hellow () {
+  return <div>Hellow组件</div>
+}
+const element = <div className='box'>
+  {/* <Hellow /> */}
+  <App />
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+</div>
+ReactDOM.render(element, document.getElementById('root'))
